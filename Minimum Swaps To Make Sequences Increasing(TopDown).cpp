6273269b9a028c1d4b70ebe286612eabee28d597
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-10-10 23:56:42 +0530

    Create Minimum Swaps To Make Sequences Increasing(TopDown).cpp

---
+++ Minimum Swaps To Make Sequences Increasing(TopDown).cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(vector<int>& nums1, vector<int>& nums2,int index, bool swapped,vector<vector<int>>&dp){
+        if(index==nums1.size()) return 0;
+        if(dp[index][swapped]!=-1) return dp[index][swapped];
+        int ans=INT_MAX;
+        int prev1=nums1[index-1];
+        int prev2=nums2[index-1];
+        
+        if(swapped){
+            swap(prev1,prev2);
+        }
+        
+        if(nums1[index]>prev1 && nums2[index]>prev2)
+            ans=min(ans,solve(nums1,nums2, index+1, 0,dp));
+        if(nums1[index]> prev2 && nums2[index]>prev1)
+            ans=min(ans,1+solve(nums1,nums2,index+1,1,dp));
+        
+        return dp[index][swapped]=ans;
+    }
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+
+        //it means that previous index were swapped or not 
+            nums1.insert(nums1.begin(),-1);
+            nums2.insert(nums2.begin(),-1);
+            bool swapped=0;
+            vector<vector<int>>dp(nums1.size()+1,vector<int>(2,-1));
+            return solve(nums1, nums2,1,swapped,dp);
+        
+        }                                                         
+};
