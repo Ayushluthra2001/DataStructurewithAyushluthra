60905057e3cf5d89de5d09fc26f32609124381a6
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-10-18 18:53:32 +0530

    Create Check if two arrays are equal or not.cpp

---
+++ Check if two arrays are equal or not.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+
+    //Function to check if two arrays are equal or not.
+    bool check(vector<ll> A, vector<ll> B, int N) {
+        map<int,int>mapping;
+        map<int,int>mapping2;
+        for(int i=0;i<N;i++){
+            mapping[A[i]]++;
+        }
+        for(int i=0;i<N;i++){
+            mapping2[B[i]]++;
+        }
+        for(int i=0;i<N;i++){
+            int no=A[i];
+            if(mapping[no]!=mapping2[no]) return false;
+        }
+       return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        
+        vector<ll> arr(n,0),brr(n,0);
+        
+        // increase the count of elements in first array
+        for(ll i=0;i<n;i++)
+            cin >> arr[i];
+        
+        
+        // iterate through another array
+        // and decrement the count of elements
+        // in the map in which frequency of elements
+        // is stored for first array
+        for(ll i=0;i<n;i++)
+            cin >> brr[i];
+        Solution ob;
+        cout << ob.check(arr,brr,n) << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
