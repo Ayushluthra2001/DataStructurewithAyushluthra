5bb3ea5a550e8586b2355f3300ab7988682fb4b4
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-11-03 18:58:30 +0530

    Create Array Removals.cpp

---
+++ Array Removals.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int removals(vector<int>& arr, int k){
+        //Code here
+        sort(arr.begin(),arr.end());
+        int i=0;
+        int j=0;
+        int n=arr.size();
+        int ans=0;
+        while(j<n){
+            if(arr[j]-arr[i]<=k){
+                
+                j++;
+                
+            }else if(i<j){
+                i++;
+            } 
+            ans=max(ans,j-i);
+        }
+        return n-ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        Solution ob;
+        int ans = ob.removals(arr,k);
+        
+        cout<<ans<<endl;
+    }
+}
+
+
+
+// } Driver Code Ends
