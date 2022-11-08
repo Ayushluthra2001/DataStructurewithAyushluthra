7180983834d150d16391404aa663125d3b034fa0
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-11-08 22:59:48 +0530

    Create Rotate matrix elements clockwise.cpp

---
+++ Rotate matrix elements clockwise.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> rotateMatrix(int m, int n, vector<vector<int>> mat) {
+        // code here
+        int row = 0, col = 0; 
+    int prev, curr; 
+
+    /* 
+    row - Starting row index 
+    m - ending row index 
+    col - starting column index 
+    n - ending column index 
+    i - iterator 
+    */
+    while (row < m && col < n) 
+    { 
+
+        if (row + 1 == m || col + 1 == n) 
+            break; 
+
+        // Store the first element of next row, this 
+        // element will replace first element of current 
+        // row 
+        prev = mat[row + 1][col]; 
+
+        /* Move elements of first row from the remaining rows */
+        for (int i = col; i < n; i++) 
+        { 
+            curr = mat[row][i]; 
+            mat[row][i] = prev; 
+            prev = curr; 
+        } 
+        row++; 
+
+        /* Move elements of last column from the remaining columns */
+        for (int i = row; i < m; i++) 
+        { 
+            curr = mat[i][n-1]; 
+            mat[i][n-1] = prev; 
+            prev = curr; 
+        } 
+        n--; 
+
+        /* Move elements of last row from the remaining rows */
+        if (row < m) 
+        { 
+            for (int i = n-1; i >= col; i--) 
+            { 
+                curr = mat[m-1][i]; 
+                mat[m-1][i] = prev; 
+                prev = curr; 
+            } 
+        } 
+        m--; 
+
+        /* Move elements of first column from the remaining rows */
+        if (col < n) 
+        { 
+            for (int i = m-1; i >= row; i--) 
+            { 
+                curr = mat[i][col]; 
+                mat[i][col] = prev; 
+                prev = curr; 
+            } 
+        } 
+        col++; 
+    } 
+    return mat;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, i, j;
+        cin >> N >> M;
+        vector<vector<int>> Mat(N, vector<int>(M));
+        for (i = 0; i < N; i++)
+            for (j = 0; j < M; j++) cin >> Mat[i][j];
+        Solution ob;
+        vector<vector<int>> ans = ob.rotateMatrix(N, M, Mat);
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < M; j++) cout << ans[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+// } Driver Code Ends
