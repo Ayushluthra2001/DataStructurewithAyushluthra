b55502707cc0390101c25f14b74783af7abb100b
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-11-12 00:34:28 +0530

    Create Leap Year.cpp

---
+++ Leap Year.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int isLeap(int N){
+        //code here
+        if(N%400==0) return 1;
+        if(N%100==0) return 0;
+        if(N%4==0) return 1;
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.isLeap(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
