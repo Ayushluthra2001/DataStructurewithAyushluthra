2df7a5da9d45a479bc46f9674ee2bf35d26c0fbc
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2022-11-18 17:31:03 +0530

    Create Median of Two Sorted Arrays(usingMergeSort).cpp

---
+++ Median of Two Sorted Arrays(usingMergeSort).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        int i=0;
+        int j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]>=nums2[j]){
+                ans.push_back(nums2[j++]);
+            }else{
+                ans.push_back(nums1[i++]);
+            }
+        }
+        while(i<nums1.size()){
+            ans.push_back(nums1[i++]);
+        }
+        while(j<nums2.size()){
+            ans.push_back(nums2[j++]);
+        }
+        int mid=ans.size()/2;
+        if(ans.size()%2!=0)
+        return ans[mid];
+        double med=ans[mid]+ans[mid-1];
+        return med/2;
+    }
+};
