c23e03abfe8f81722c9c056180a7a9dbd1289814
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-02-04 00:25:24 +0530

    Create Third largest element(O(N)).cpp

---
+++ Third largest element(O(N)).cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+         //Your code here
+         int first=a[0],second=0,third=0;
+         for(int i=1;i<n;i++){
+             if(a[i]>first){
+                 third=second;
+                 second=first;
+                 first=a[i];
+             }else if(a[i]>second){
+                 third=second;
+                 second=a[i];
+             }else if(a[i]>third){
+                 third=a[i];
+             }
+         }
+         return third;
+    }
+
+};
+
+//{ Driver Code Starts.
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	   Solution obj;
+	    cout<<obj.thirdLargest(a,n)<<endl;
+    }
+}   
+// } Driver Code Ends
