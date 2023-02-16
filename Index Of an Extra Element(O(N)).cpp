887bd615b5b65201569ea7e5cfabdcc9682b8132
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-02-16 17:44:26 +0530

    Create Index Of an Extra Element(O(N)).cpp

---
+++ Index Of an Extra Element(O(N)).cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int findExtra(int a[], int b[], int n) {
+        // add code here.
+        int index=-1;
+
+        int i=0,j=0;
+        while(i<n && j<n){
+            if(a[i]<b[j]) return i;
+            i++;
+            j++;
+        }
+        return n-1;
+            
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], b[n - 1];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n - 1; i++) {
+            cin >> b[i];
+        }
+        Solution obj;
+        cout << obj.findExtra(a, b, n) << endl;
+    }
+}
+// } Driver Code Ends
