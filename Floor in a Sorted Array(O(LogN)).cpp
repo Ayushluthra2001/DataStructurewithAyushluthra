f34c2ef8d215391bc2feff02521755f8807fbf7d
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-02-21 00:11:16 +0530

    Create Floor in a Sorted Array(O(LogN)).cpp

---
+++ Floor in a Sorted Array(O(LogN)).cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> arr, long long n, long long x){
+        
+       int start=0,end=n-1,index=-1;
+        int mid=start+(end-start)/2;    
+        while(start<=end){
+            if(arr[mid]==x){
+                return mid;
+            }else if(arr[mid]<x){
+                index=mid;
+                start=mid+1;
+            }else {
+                end=mid-1;
+            }
+            mid=start+(end-start)/2;
+        }
+        return index;        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
