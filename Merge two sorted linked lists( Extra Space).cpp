6f36915e48ec454e8ec4b5af3f6b795201091adf
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-03-16 17:50:52 +0530

    Create Merge two sorted linked lists( Extra Space).cpp

---
+++ Merge two sorted linked lists( Extra Space).cpp
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+ 
+
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+  
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+//Function to merge two sorted linked list.
+#include<bits/stdc++.h>
+Node* sortedMerge(Node* head1, Node* head2)  
+{  
+    // code here
+    vector<int>list;
+    Node* temp=head1;
+    while(temp!=NULL){
+        list.push_back(temp->data);
+        temp=temp->next;
+    }
+     temp=head2;
+    while(temp!=NULL){
+        list.push_back(temp->data);
+        temp=temp->next;
+    }
+    sort(list.begin(),list.end());
+    Node* dummy = new Node(-1);
+    temp=dummy;
+    for(int i=0;i<list.size();i++){
+        temp->next=new Node(list[i]);
+        temp=temp->next;
+    }
+    return dummy->next;
+}  
