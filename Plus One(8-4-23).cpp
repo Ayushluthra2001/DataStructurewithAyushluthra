e45d385c26d297293f530311ca585f809cd86a7c
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-04-08 20:17:40 +0530

    Create Plus One(8-4-23).cpp

---
+++ Plus One(8-4-23).cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> increment(vector<int> arr ,int N) {
+        // code here
+        int carry=1;
+        int i=N-1;
+        
+        vector<int>ans;
+        while(carry || i>=0 ){
+            int x=i>=0 ? arr[i] : 0 ;
+            int sum=x+carry;
+            
+            ans.push_back(sum%10);
+            carry=sum/10;
+           
+            i--;
+            
+            
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        char c;
+        
+        cin>>N;
+        vector<int> arr(N);
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        vector<int> res = ob.increment(arr,N);
+        for(int i: res)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
