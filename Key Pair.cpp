eefb100d201b97a21f33a8ca72b3ab41c8426087
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2023-04-20 23:38:42 +0530

    Create Key Pair.cpp

---
+++ Key Pair.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    unordered_map<int,int>mapping;
+	    for(int i=0;i<n;i++){
+	        int rem= x-arr[i];
+	        
+	        if(mapping.find(rem)!=mapping.end()) return true;
+	        else mapping[arr[i]]=i;
+	    }
+	  
+	    return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
