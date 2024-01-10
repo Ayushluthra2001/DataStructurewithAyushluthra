738273b2ac2db33f0988c4c6f6734985e9740f1c
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-01-11 01:08:55 +0530

    Create Equilibrium Point.cpp

---
+++ Equilibrium Point.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+        
+        long long  sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
+        int currsum=0;
+        for(int i=0;i<n;i++){
+            int leftsum=sum-a[i]-currsum;
+            if(leftsum==currsum)return i+1;
+            currsum+=a[i];
+        }
+        
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
