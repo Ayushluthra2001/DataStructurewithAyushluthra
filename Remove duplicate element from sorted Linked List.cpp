d79ff8008e59f79aef382ce17837370176663922
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-01-12 20:36:45 +0530

    Create Remove duplicate element from sorted Linked List.cpp

---
+++ Remove duplicate element from sorted Linked List.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+//Function to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+    Node* ans=new Node(-1);
+    Node* temp=head;
+    Node* temp2=ans;
+    while(temp!=NULL && temp->next!=NULL){
+       
+        while(temp->next!=NULL && temp->data==temp->next->data)
+        temp=temp->next;
+         temp2->next=new Node(temp->data);
+        temp2=temp2->next;
+        temp=temp->next;
+    }
+    if(temp!=NULL){
+        temp2->next=new Node(temp->data);
+        temp2=temp2->next;
+    }
+    return ans->next;
+}
