552eca897e84e877632d4ac39288a72a4a71caac
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-02-07 20:49:14 +0530

    Create Sub or Swp.cpp

---
+++ Sub or Swp.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int x;
+	    int y;
+	    cin>>x>>y;
+	    cout<<__gcd(x,y)<<endl;
+	}
+	return 0;
+
+}
