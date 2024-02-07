de09c93dc97ae2eaa38de6b894bbded725223e6b
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-02-07 20:13:38 +0530

    Create Valentine Gifts.cpp

---
+++ Valentine Gifts.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int x;
+	    cin>>x;
+	    if(x>=127){
+	        cout<<"Yes"<<endl;
+	    }else{
+	        cout<<"No"<<endl;
+	    }
+	   
+	}
+	return 0;
+	
+
+}
