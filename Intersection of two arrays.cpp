2810d620feec921747a5ebde220a1166e2e16045
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-08-03 02:19:58 +0530

    Create Intersection of two arrays.cpp

---
+++ Intersection of two arrays.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+        unordered_map<int,int>mapping1;
+        int count = 0;
+        for(int i=0; i<n; i++){
+            mapping1[a[i]] ++;
+            
+        }
+        for(int i=0; i<m; i++){
+            if(mapping1.find(b[i]) != mapping1.end()) {
+                mapping1.erase(b[i]);
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a[n], b[m];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        for (int i = 0; i < m; i++) cin >> b[i];
+        Solution ob;
+        cout << ob.NumberofElementsInIntersection(a, b, n, m) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
