a100dce6d5076034ffe29724462e2a2ec464b955
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-08-14 22:19:33 +0530

    Create Truth Teller And Liars 101.cpp

---
+++ Truth Teller And Liars 101.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ;
+	cin>>t;
+	while(t--){
+	    int a , b;
+	    cin>>a>>b;
+	    
+	    if(b>=a) cout<<-1<<endl;
+	    else cout<<b + b + 1 <<endl;
+	    
+	
+	  
+	}
+
+}
