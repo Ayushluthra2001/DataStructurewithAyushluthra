34cca1b5c64215b19f1ce1c6c137cb50eee22134
Ayush Luthra <71180629+Ayushluthra2001@users.noreply.github.com>
2024-08-14 22:18:02 +0530

    Create Gold Coins 101.cpp

---
+++ Gold Coins 101.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a , b, c, d;
+	cin>>a>>b>>c>>d;
+	
+	if( c > d) cout<<a <<endl;
+	else cout<<b<<endl;
+
+}
